841bcc08e4458856fc6b69a0087589dde4c9076d
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-11-07 22:52:13 +0200

    Add ContestsFilter component for refactoring CreatorDashboard

---
+++ client/src/components/CreatorDashboard/ContestsFilter/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {useDispatch} from 'react-redux';
+import * as ContestActionCreators from '../../../actions/contestActionCreators';
+import styles from '../../CustomerDashboard/CustomerDashboard.module.sass';
+
+const ContestsFilter = (props) => {
+
+  const dispatch = useDispatch();
+  const {status, title, currentFilter} = props;
+
+  return (
+      <div
+          onClick={() =>
+              dispatch(ContestActionCreators.setNewCustomerFilter(status))
+          }
+
+          className={
+            status === currentFilter
+                ? styles.activeFilter
+                : styles.filter}
+      >
+        {title}
+      </div>);
+};
+
+export default ContestsFilter;
