178477ede8440d9a92f6276fe23987a925f2c8a7
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-11-11 18:47:39 +0200

    Add singleContest action types with creators

---
+++ client/src/actions/singleContestActionCreators.js
@@ -0,0 +1,26 @@
+import SINGLE_CONTEST_ACTION_TYPES from './singleContestActionTypes';
+
+export const getDescriptionForContest = values => ({
+  type: SINGLE_CONTEST_ACTION_TYPES.GET_DESCRIPTION_FOR_CONTEST,
+  payload: {
+    values,
+  },
+});
+
+export const getDescriptionForContestRequest = () => ({
+  type: SINGLE_CONTEST_ACTION_TYPES.GET_DESCRIPTION_FOR_CONTEST_REQUEST,
+});
+
+export const getDescriptionForContestSuccess = values => ({
+  type: SINGLE_CONTEST_ACTION_TYPES.GET_DESCRIPTION_FOR_CONTEST_SUCCESS,
+  payload: {
+    values,
+  },
+});
+
+export const getDescriptionForContestFailed = error => ({
+  type: SINGLE_CONTEST_ACTION_TYPES.GET_DESCRIPTION_FOR_CONTEST_FAILED,
+  payload: {
+    error,
+  },
+});
