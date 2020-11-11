b3cec0060c950e3c59be68a3cba75efb0fbd188f
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-11-11 20:10:31 +0200

    Fix description loading of singleContestRouter

--- server/routes/singleContest.js
+++ server/routes/singleContest.js
@@ -1,6 +1,6 @@
 const singleContestRouter = require('express').Router();
 const controller = require('../controllers/contestController');
 
-singleContestRouter.get('/description', controller.getIndustriesForContest);
+singleContestRouter.get('/description', controller.getDescriptionForContest);
 
 module.exports = singleContestRouter;
