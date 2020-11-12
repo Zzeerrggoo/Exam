70a1bd4497bc8cb770a08ff4c980e7194c46e409
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-11-12 18:16:55 +0200

    Add payment API

---
+++ client/src/api/http/PaymentApi.js
@@ -0,0 +1,19 @@
+class PaymentApi {
+  #_client;
+
+  constructor({client}) {
+    this.#_client = client;
+    this.url = '/payment';
+  }
+
+  pay(data) {
+    return this.#_client.patch(`${this.url}/pay`, data);
+  }
+
+  cashout(data) {
+    return this.#_client.patch(`${this.url}/cashout`, data);
+  }
+
+}
+
+export default PaymentApi;
