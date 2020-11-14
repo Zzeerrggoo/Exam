b5902463b379d8190edc8bfe8e6faed4f1ba11f0
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-11-14 21:51:27 +0200

    Refactor

--- client/src/api/rest/restController.js
+++ client/src/api/rest/restController.js
@@ -1,18 +1,8 @@
 import http from '../interceptor';
 import client from '../http';
 
-export const getUser = () => http.post('getUser');
-export const updateContest = (data) => http.post('updateContest', data);
-export const setNewOffer = (data) => http.post('setNewOffer', data);
-export const setOfferStatus = (data) => http.post('setOfferStatus', data);
-export const downloadContestFile = (data) =>
-    http.get('downloadFile/' + data.fileName);
-export const payMent = (data) => client.post('pay', data.formData);
-export const changeMark = (data) => http.post('changeMark', data);
 export const getPreviewChat = () => http.post('getPreview');
 export const getDialog = (data) => http.post('getChat', data);
-//export const dataForContest = (data) => http.post('dataForContest', data);
-export const cashOut = (data) => client.post('cashout', data);
 export const newMessage = (data) => http.post('newMessage', data);
 export const changeChatFavorite = (data) => http.post('favorite', data);
 export const changeChatBlock = (data) => http.post('blackList', data);
@@ -24,12 +14,18 @@ export const deleteCatalog = (data) => http.post('deleteCatalog', data);
 export const removeChatFromCatalog = (data) =>
     http.post('removeChatFromCatalog', data);
 export const changeCatalogName = (data) => http.post('updateNameCatalog', data);
-//THAT'S IT
 
+/*//////////////////THAT'S IT//////////////////*/
 export const getContestById = (data) => {
-  return http.get('getContestById', {
+  return client.get('getContestById', {
     headers: {
       contestId: data.contestId,
     },
   });
 };
+export const updateContest = (data) => client.post('updateContest', data);
+export const setNewOffer = (data) => client.post('setNewOffer', data);
+export const setOfferStatus = (data) => client.post('setOfferStatus', data);
+export const downloadContestFile = (data) =>
+    client.get('downloadFile/' + data.fileName);
+export const changeMark = (data) => client.post('changeMark', data);
