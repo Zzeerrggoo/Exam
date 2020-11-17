4fb6100f11db6aaa2dc1276ff1f17b94b1341ff5
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-11-17 13:57:57 +0200

    Update singleContest router

--- server/routes/singleContest.js
+++ server/routes/singleContest.js
@@ -1,10 +1,12 @@
 const singleContestRouter = require('express').Router();
 const controller = require('../controllers/contestController');
 const basicMiddlewares = require('../middlewares/basicMiddlewares');
+const upload = require('../utils/fileUpload');
 
 singleContestRouter.get('/description', controller.getDescriptionForContest);
 
 singleContestRouter.route('/:contestId')
-  .get(basicMiddlewares.canGetContest, controller.getContestDataById);
+  .get(basicMiddlewares.canGetContest, controller.getContestDataById)
+  .patch(upload.updateContestFile, controller.updateContest);
 
 module.exports = singleContestRouter;
