5aee5b3a72dac561be433957d7ed967254407109
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-11-22 19:47:28 +0200

    Add the rest of offer action types with creators

--- client/src/actions/offerActionCreators.js
+++ client/src/actions/offerActionCreators.js
@@ -12,7 +12,7 @@ export const getOffersForContestRequest = () => ({
 });
 
 export const getOffersForContestSuccess = values => ({
-  type:OFFER_ACTION_TYPES.GET_OFFERS_FOR_CONTEST_SUCCESS,
+  type: OFFER_ACTION_TYPES.GET_OFFERS_FOR_CONTEST_SUCCESS,
   payload: {
     values,
   },
@@ -20,6 +20,81 @@ export const getOffersForContestSuccess = values => ({
 
 export const getOffersForContestFailed = error => ({
   type: OFFER_ACTION_TYPES.GET_OFFERS_FOR_CONTEST_FAILED,
+  payload: {
+    error,
+  },
+});
+
+export const changeOfferMark = values => ({
+  type: OFFER_ACTION_TYPES.CHANGE_OFFER_MARK,
+  payload: {
+    values,
+  },
+});
+
+export const changeOfferMarkSuccess = values => ({
+  type: OFFER_ACTION_TYPES.CHANGE_OFFER_MARK_SUCCESS,
+  payload: {
+    values,
+  },
+});
+
+export const changeOfferMarkFailed = error => ({
+  type: OFFER_ACTION_TYPES.CHANGE_OFFER_MARK_FAILED,
+  payload: {
+    error,
+  },
+});
+
+export const clearMarkError = () => ({
+  type: OFFER_ACTION_TYPES.CLEAR_MARK_ERROR,
+});
+
+export const addNewOffer = values => ({
+  type: OFFER_ACTION_TYPES.ADD_NEW_OFFER,
+  payload: {
+    values,
+  },
+});
+
+export const addNewOfferRequest = () => ({
+  type: OFFER_ACTION_TYPES.ADD_NEW_OFFER_REQUEST,
+});
+
+export const addNewOfferSuccess = values => ({
+  type: OFFER_ACTION_TYPES.ADD_NEW_OFFER_SUCCESS,
+  payload: {
+    values,
+  },
+});
+
+export const addNewOfferFailed = error => ({
+  type: OFFER_ACTION_TYPES.ADD_NEW_OFFER_FAILED,
+  payload: {
+    error,
+  },
+});
+
+export const setOfferStatus = values => ({
+  type: OFFER_ACTION_TYPES.SET_OFFER_STATUS,
+  payload: {
+    values,
+  },
+});
+
+export const setOfferStatusRequest = () => ({
+  type: OFFER_ACTION_TYPES.SET_OFFER_STATUS_REQUEST,
+});
+
+export const setOfferStatusSuccess = values => ({
+  type: OFFER_ACTION_TYPES.SET_OFFER_STATUS_SUCCESS,
+  payload: {
+    values,
+  },
+});
+
+export const setOfferStatusFailed = error => ({
+  type: OFFER_ACTION_TYPES.SET_OFFER_STATUS_FAILED,
   payload: {
     error,
   },
