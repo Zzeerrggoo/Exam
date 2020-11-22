88ea4d29766f9e2b574d94ddea15b50ab63755d0
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-11-22 19:52:42 +0200

    Expand OffersApi according to new action types

--- client/src/api/http/OffersApi.js
+++ client/src/api/http/OffersApi.js
@@ -11,6 +11,19 @@ class OffersApi {
     return this.#_client.get(`${this.url}/${contestId}`);
   };
 
+  addNewOffer = (data) => {
+    return this.#_client.post(`${this.url}/`, data);
+  };
+
+  setOfferStatus = (data) => {
+    const urlParam = new URLSearchParams(data.status).toString();
+    return this.#_client.get(`${this.url}/:${data.offerId}/status?${urlParam}`);
+  };
+
+  changeOfferMark = (data) => {
+    return this.#_client.put(`${this.url}/:${data.offerId}/rating`, data);
+  };
+
 }
 
 export default OffersApi;
