fc1de7a5b21becf8dce91d45611065758724f7b0
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-11-22 19:45:23 +0200

    Add offer route for changing mark

--- server/routes/offers.js
+++ server/routes/offers.js
@@ -1,6 +1,10 @@
 const offersRouter = require('express').Router();
 const controller = require('../controllers/offersController');
+const basicMiddlewares = require('../middlewares/basicMiddlewares');
 
 offersRouter.get('/:contestId', controller.getOffersForContest);
+
+offersRouter.put('/:offerId/rating', basicMiddlewares.onlyForCustomer,
+  controller.changeMark);
 
 module.exports = offersRouter;
