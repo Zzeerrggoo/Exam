52111fcfcca0d68cef49d871d5aa0a6626dea121
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-11-28 03:11:12 +0200

    Add postNewMessage controller

--- server/controllers/newChatcontroller.js
+++ server/controllers/newChatcontroller.js
@@ -44,3 +44,59 @@ module.exports.getChatsPreview = async (req, res, next) => {
     next(err);
   }
 };
+
+module.exports.postNewMessage = async (req, res, next) => {
+  const senderId = req.tokenPayload.userId;
+  const recipientId = req.body.recipient;
+  const tryChatId = req.body?.chatId ?? null;
+  const { messageBody } = req.body;
+
+  try {
+    const chat = await Chat.findOrCreate({
+      where: {
+        id: tryChatId,
+      },
+    });
+
+    const chatId = chat[0].dataValues.id;
+
+    const userChat = await UserChat.findOrCreate({
+      where: {
+        chatId,
+        userId: senderId,
+      },
+      returning: true,
+    });
+    const chatData = userChat[0].get({ plain: true });
+
+    const newMessage = await Message.create({
+      userId: senderId,
+      chatId,
+      body: messageBody,
+    });
+    const message = newMessage.get({ plain: true });
+
+    const interlocutor = await User.findOne({
+      where: {
+        id: recipientId,
+      },
+      attributes: {
+        exclude: ['role', 'balance', 'password'],
+      },
+      raw: true,
+    });
+
+    const data = {
+      chatData,
+      message,
+      interlocutor,
+    };
+
+    controller.getChatController().emitNewMessage(recipientId, data);
+    res.status(201).send({ data });
+  } catch (err) {
+    next(err);
+  }
+};
+
+/// ///////////////////////////////////////////////////////////
