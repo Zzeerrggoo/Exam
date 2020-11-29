3455fc4a38f536fba649a066b69bed090c92e9c0
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-11-30 00:20:21 +0200

    Update changeChatBlock saga

--- client/src/sagas/chatSagas.js
+++ client/src/sagas/chatSagas.js
@@ -49,31 +49,32 @@ export function* sendMessage(action) {
   }
 }
 
-/////////////////////////////////////////
-
 export function* getDialog(action) {
   try {
-    const {data} = yield restController.getDialog(action.data);
+    const {data: {data}} = yield Api.chats.getChat(action.data);
     yield put({type: ACTION.GET_DIALOG_MESSAGES, data: data});
   } catch (err) {
     yield put({type: ACTION.GET_DIALOG_MESSAGES_ERROR, error: err.response});
   }
 }
 
-export function* changeChatFavorite(action) {
+export function* changeChatBlock(action) {
   try {
-    const {data} = yield restController.changeChatFavorite(action.data);
+    const {data: {data}} = yield Api.chats.setChatBlocked(action.data);
+    console.log(data);
     const {messagesPreview} = yield select(state => state.chatStore);
     messagesPreview.forEach(preview => {
-      if (isEqual(preview.participants, data.participants))
-        preview.favoriteList = data.favoriteList;
+      if (preview.chatId === data.chatId) {
+        preview.isBlocked = data.isBlocked;
+        preview.isInBlackList = data.isInBlackList;
+      }
     });
     yield put({
-      type: ACTION.CHANGE_CHAT_FAVORITE,
-      data: {changedPreview: data, messagesPreview},
+      type: ACTION.CHANGE_CHAT_BLOCK,
+      data: {messagesPreview, chatData: data},
     });
   } catch (err) {
-    yield put({type: ACTION.SET_CHAT_FAVORITE_ERROR, error: err.response});
+    yield put({type: ACTION.SET_CHAT_BLOCK_ERROR, error: err.response});
   }
 }
 
@@ -83,7 +84,7 @@ export function* changeChatBlock(action) {
     const {messagesPreview} = yield select(state => state.chatStore);
     messagesPreview.forEach(preview => {
       if (isEqual(preview.participants, data.participants))
-        preview.blackList = data.blackList;
+        preview.favoriteList = data.favoriteList;
     });
     yield put({
       type: ACTION.CHANGE_CHAT_BLOCK,
