a22393ff8ca154f50ddf551e2f12f54ad93fc1a8
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-12-05 20:41:40 +0200

    Add MaterialUiPagination component for pagination

---
+++ client/src/components/MaterialUiPagination/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Pagination} from '@material-ui/lab';
+import {makeStyles} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  customPagination: {
+    alignSelf: 'stretch',
+    paddingTop: '3rem',
+    paddingBottom: '5rem',
+    '& .MuiPagination-ul': {
+      justifyContent: 'center',
+    },
+    '& .MuiPaginationItem-sizeLarge': {
+      fontSize: '1.5rem',
+    },
+  },
+});
+
+const MaterialUiPagination = (props) => {
+
+  const {pagesNumber, siblingCount, onPageChange} = props;
+  const classes = useStyles();
+
+  const handleChange = (target, page) => {
+    onPageChange(page);
+  };
+
+  return (<Pagination count={pagesNumber ?? 0}
+                      siblingCount={siblingCount ?? 2}
+                      shape="rounded"
+                      size="large"
+                      classes={{root: classes.customPagination}}
+                      onChange={handleChange}
+                      getItemAriaLabel={(data) => data}
+  />);
+};
+
+export default MaterialUiPagination;
