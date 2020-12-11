8ee808874addd60f8159c1dbecce86ef2c525917
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-12-11 18:54:46 +0200

    Add EventsPage

---
+++ client/src/pages/EventsPage/index.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import BrandingList from '../../components/BrandingList';
+import BrandingForm from '../../components/forms/BrandingForm';
+import SelectModeAside from '../../components/SelectModeAside';
+import styles from './EventsPage.module.scss'
+
+const modes = {
+  list: 'list',
+  form: 'form',
+};
+
+const EventsPage = () => {
+
+  const [mode, setMode] = useState(modes.list);
+
+  const changeFilter = (newFilter) => {
+    setMode(newFilter);
+  };
+
+  return (
+      <>
+        <Header/>
+        <div className={styles.container}>
+          <SelectModeAside changeFilter={changeFilter}
+                           options={[
+                             {text: 'List', filter: modes.list},
+                             {text: 'Add new', filter: modes.form}]}/>
+          {mode === modes.list ? <BrandingList/> : <BrandingForm/>}
+        </div>
+        <Footer/>
+      </>
+  );
+};
+
+export default EventsPage;
