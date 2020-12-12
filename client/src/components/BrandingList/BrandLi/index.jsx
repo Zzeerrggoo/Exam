53787ede59f587c17804ae2dedc4a639b3c13ff1
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-12-12 10:14:25 +0200

    Add deleting button for BrandingLi

--- client/src/components/BrandingList/BrandLi/index.jsx
+++ client/src/components/BrandingList/BrandLi/index.jsx
@@ -2,26 +2,58 @@ import React from 'react';
 import * as date from 'date-fns';
 import {useContext} from 'react';
 import {CurrentTime} from '../../GeneralCounter';
+import classNames from 'classnames';
+import styles from './BrandLi.module.scss';
 
 const BrandLi = props => {
 
-  const {currentTime} = useContext(CurrentTime);
+  const {currentTime, expiredTimers, tickingTimers} = useContext(CurrentTime);
   const {start, end, description, isExpired} = props;
+
   const fullTime = date.differenceInMilliseconds(new Date(end),
       new Date(start));
   const value = isExpired ? fullTime : date.differenceInMilliseconds(
       currentTime,
       new Date(start));
+  const timeLeft = isExpired ? 'Finished' : date.formatDistanceStrict(fullTime,
+      value);
+
+  const progressId = `${description}${start}`;
+  const progressClassName = classNames(styles.brandingProgress,
+      {[styles.brandingProgressFinished]: isExpired});
+
+  const handleDelete = () => {
+
+    const searchArray = isExpired ? expiredTimers : tickingTimers;
+    const index = searchArray.findIndex(item => item.start === start);
+    if (index !== -1) {
+      searchArray.splice(index, 1);
+    }
+
+    window.localStorage.setItem('counters',
+        JSON.stringify({expiredTimers, tickingTimers}));
+
+  };
 
   return (
-      <div>
-        <label>{description}
-          <progress
-              max={fullTime}
-              value={value}>{value}
-          </progress>
-        </label>
-      </div>
+      <li className={styles.container}>
+        <label htmlFor={progressId}
+               className={styles.brandingLabel}>{description}</label>
+        <progress id={progressId}
+                  max={fullTime}
+                  value={value}
+                  className={progressClassName}/>
+        <span className={styles.expiringTime}>{timeLeft}</span>
+
+        <button className={styles.deleteButton}
+                onClick={handleDelete}>
+          <svg
+              viewBox="0 0 24 24">
+            <path fill="currentColor"
+                  d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"/>
+          </svg>
+        </button>
+      </li>
   );
 
 };
