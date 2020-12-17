e2f1c93a3297bc7ebc5cfd86ea4e976b018cfcb9
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-12-17 19:46:16 +0200

    Make error field in project's style

--- client/src/components/forms/PasswordRestoreForm/FormField/index.jsx
+++ client/src/components/forms/PasswordRestoreForm/FormField/index.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import {useField} from 'formik';
+import classNames from 'classnames';
 import styles from './FormField.module.scss';
 
 function FormField(props) {
   const [field, meta] = useField(props);
   const {error, touched} = meta;
   const isError = Boolean(error && touched);
+  const inputClassName = classNames(styles.input, {[styles.inputError]: isError});
 
   return (
       <div className={styles.container}>
         <input
             {...field}
             {...props}
-            className={styles.input}
+            className={inputClassName}
             title={props.placeholder ?? ''}
         />
         {isError && <div className={styles.errorMessage}>{error}</div>}
